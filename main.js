0e69a9feb49d9dfa6c7acdb2946c936b56c81fd6
DannyGalvan <cgalvan29111999@gmail.com>
2021-10-10 14:46:57 -0600

    seccion paquetes

--- main.js
+++ main.js
@@ -1,23 +1,12 @@
 $(function(){
 
+    $("#inlineRadio1").change(function(){
+        $("#contenedor2 div").fadeIn();
+        $("#imagencaja").fadeIn();
+    })
+
     $("#inlineRadio2").change(function(){
-        var hola =  $("#inlineRadio2").attr("value");
-        alert("usted ha seleccionado" + " " + hola);
-        console.log(hola);
-    })
-
-    $("#agregando").click(function(){
-        $("#agregando").after("<button>hola</button>");
-        $("button").addClass("btn btn-forza");
-    })
-
-   
+        $("#contenedor2 div").fadeOut();
+        $("#imagencaja").fadeOut();
+    })  
 })
-
-var j = document.getElementById("inlineRadio2");
-j.addEventListener("change",saludo);
-
-function saludo() 
-{
-    alert("hola " + j.value)
-}
