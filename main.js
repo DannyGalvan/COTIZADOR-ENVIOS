5408a5be707bea17eb53eaad004a1cc3119bc066
DannyGalvan <cgalvan29111999@gmail.com>
2021-11-20 15:11:38 -0600

    Calculos Totales

--- main.js
+++ main.js
@@ -33,23 +33,44 @@ const ancho = document.getElementById('ancho');
 const largo = document.getElementById('largo');
 const peso = document.getElementById('peso');
 const tabla = document.getElementById('tabla');
+const tabla2 = document.getElementById('tabla2');
 
 const agregar = document.getElementById('agregarFormulario');
 agregar.addEventListener("click",agg);
+
+const eliminarPocicion = document.getElementById('eliminarPocicion');
+const eliminarFormulario = document.getElementById('eliminarFormulario');
+eliminarFormulario.addEventListener('click',eliminar)
+
+const modificarPocicion = document.getElementById('modificarPocicion');
+const modificarFormulario = document.getElementById('modificarFormulario');
+modificarFormulario.addEventListener('click',modificar)
+
+const departamentoOrigen = document.getElementById('departamentoSelectOrigen');
+const departamentoDestino = document.getElementById('departamentoSelectDestino');
+
+const totalizador = document.getElementById('totalizador');
+totalizador.addEventListener('click',calcularTotal)
 
 
 var paquet = [];
 
  function agg() {
-     if (inputradio1.checked) 
- {
-     agregandoCaja();
-     print();
- }
- else{
-     agregandoSobre();
-     print();
- }
+        if (peso.value <= 0 || alto.value <= 0 || ancho.value <= 0 || largo.value <= 0) {
+            alert("ingrese todos los datos de su paquete")
+        }
+        else{
+            if (inputradio1.checked) 
+            {
+            agregandoCaja();
+            print();  
+            }
+            else{
+            agregandoSobre();
+            print();
+            }
+        }
+        
  }
 
 function agregandoSobre() {
@@ -66,15 +87,31 @@ function print()
     let contador = 1;
     let suma = 0;
     let resultado = 0;
+    let dimension = 0;
+    let pesado = 0;
     for (const elementos of paquet) 
     {
-        resultado = ((parseFloat(elementos.peso) - 30) * 0.90);
+        dimension = ((elementos.largo * elementos.ancho * elementos.alto)/5000) * 2.20;
+        pesado = elementos.peso;
+        
+        if (dimension > pesado)
+        {
+            resultado = ((parseFloat(dimension) - 30) * 0.90);
+            console.log( "dimension " +resultado)
+        }
+        else
+        {
+            resultado = ((parseFloat(pesado) - 30) * 0.90);
+            console.log("pesado " + resultado)
+        }
+
+        let parar = resultado + 30
         let valor = 17;
-        for (let index = 0; index < elementos.peso; index++) {
-            if (elementos.peso <= 30) {
+        for (let index = 0; index < parar; index++) {
+            if (parar <= 30) {
                 resultado = 0;
             }
-            if (elementos.peso < 37) {
+            if (parar < 37) {
                 resultado = resultado;
             }
             else
@@ -102,10 +139,106 @@ function print()
     } 
     
     tabla.innerHTML += `<td></td>` + `<td></td>` + `<td></td>` + `<td></td>` + `<td>Total: </td>` + `<td>${suma.toFixed(2)}</td>`;
+    tabla2.style.display = "none";
 }
 
+function eliminar() 
+{
+    position = eliminarPocicion.value -1;
+    if (position >= 0)
+    {
+        paquet.splice(position,1);
+        print();
+        eliminarPocicion.value = " ";
+    }
+    else{
+        paquet.pop();
+        print();
+    }
+}
 
+function modificar() 
+{
+    position = modificarPocicion.value - 1;
 
+    if (position >= 0 & inputradio2.checked) 
+    {
+        paquet.splice(position,1,new sobre("sobre",peso.value));
+        print();
+        modificarPocicion.value = " ";
+    }
 
+    if (position >= 0 & inputradio1.checked) 
+    {
+        paquet.splice(position,1,new caja("caja",peso.value,largo.value,ancho.value,alto.value));
+        print();
+        modificarPocicion.value = " ";
+    }
+    else{
+        alert("ingrese una pocicion")
+    }
+}
+
+function calcularTotal() 
+{
+    let totalDistancia = 0;
+
+    if (departamentoDestino.value == departamentoOrigen.value) 
+    {
+        totalDistancia = 25 * paquet.length;
+    }
+    else{
+        totalDistancia = 35 * paquet.length;
+    }
+
+    let suma = 0;
+    let resultado = 0;
+    let dimension = 0;
+    let pesado = 0;
+
+    for (const elementos of paquet) 
+    {
+        dimension = ((elementos.largo * elementos.ancho * elementos.alto)/5000) * 2.20;
+        pesado = elementos.peso;
+        
+        if (dimension > pesado)
+        {
+            resultado = ((parseFloat(dimension) - 30) * 0.90);
+            console.log( "dimension " +resultado)
+        }
+        else
+        {
+            resultado = ((parseFloat(pesado) - 30) * 0.90);
+            console.log("pesado " + resultado)
+        }
+
+        let parar = resultado + 30
+        let valor = 17;
+        for (let index = 0; index < parar; index++) {
+            if (parar <= 30) {
+                resultado = 0;
+            }
+            if (parar < 37) {
+                resultado = resultado;
+            }
+            else
+            {
+                if (index == valor){
+                    resultado = resultado - 0.10;
+                    valor = valor + 10;
+                    if (valor > resultado) {
+                        break
+                    }
+                }
+            }   
+        }
+        suma = suma + resultado;
+    }
+    const descuento = (suma + totalDistancia) * 0.1;
+    const granTotal = totalDistancia + suma - descuento;
+    tabla2.style.display = "table";
+    tabla2.innerHTML = `<tr><th>TOTALES</th><td>Cifras en (Q)</td>` + `<tr><th>tarifa total envio</th><td>${totalDistancia.toFixed(2)}</td>` + `<tr><th>tarifa total Recargo peso</th><td>${suma.toFixed(2)}</td>` + `<tr><th>Descuento del 10%</th><td>${descuento.toFixed(2)}</td>` + `<tr><th>Total a Pagar</th><td>${granTotal.toFixed(2)}</td>`;
+
+}
 
 
